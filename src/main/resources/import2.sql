0c48d4677affe0a39623402354327d8f45c8f222
declanmcgee01 <declanmcgee1995@gmail.com>
2019-01-18 12:24:28 +0000

    Gave dummy data to classroom and trainee through sql files

---
+++ src/main/resources/import2.sql
@@ -0,0 +1,5 @@
+INSERT INTO TRAINEE (traineeID, traineeName) VALUES (1, 'Declan')
+
+INSERT INTO TRAINEE (traineeID, traineeName) VALUES (2, 'Umayr')
+
+INSERT INTO TRAINEE (traineeID, traineeName) VALUES (3, 'Jacob')
