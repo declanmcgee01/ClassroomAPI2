9d026fe7edecef872349f60890b13e8ff1f1c4ce
declanmcgee01 <declanmcgee1995@gmail.com>
2019-01-18 14:42:15 +0000

    conflict fix

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,6 @@
 INSERT INTO CLASSROOM (classroomID, trainer) VALUES (1, 'John')
 INSERT INTO CLASSROOM (classroomID, trainer) VALUES (2, 'Matt')
 INSERT INTO CLASSROOM (classroomID, trainer) VALUES (3, 'Jordan')
-INSERT INTO TRAINEE (traineeID, traineeName) VALUES (1, 'Declan')
-INSERT INTO TRAINEE (traineeID, traineeName) VALUES (2, 'Umayr')
-INSERT INTO TRAINEE (traineeID, traineeName) VALUES (3, 'Jacob')
+
 
 
