ffda3d514d632aaff11ae7ab3bde201b951356c0
declanmcgee01 <declanmcgee1995@gmail.com>
2019-01-18 10:32:01 +0000

    created classroom entity

--- src/main/java/com/qa/persistence/domain/Classroom.java
+++ src/main/java/com/qa/persistence/domain/Classroom.java
@@ -1,5 +1,41 @@
 package com.qa.persistence.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Classroom {
+	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	
+	private Long classroomId;
+	private String trainer;
+	
+	public Classroom() {
+		
+	}
+	
+	public Classroom(String trainer) {
+		this.trainer = trainer;
+	}
+
+	public Long getClassroomId() {
+		return classroomId;
+	}
+
+	public void setClassroomId(Long classroomId) {
+		this.classroomId = classroomId;
+	}
+
+	public String getTrainer() {
+		return trainer;
+	}
+
+	public void setTrainer(String trainer) {
+		this.trainer = trainer;
+	}
 
 }
